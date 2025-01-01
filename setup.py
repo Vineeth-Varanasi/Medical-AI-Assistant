39e6f3f837c5d85c0c3659b98569917280d929e6
vin var <vineethvaranasi06@gmail.com>
2025-01-01 17:24:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup (
+    name = 'Gen AI Project',
+    version = '0.0.0',
+    author = 'Vineeth Varanasi',
+    author_email = 'vineethvaranasi06@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
